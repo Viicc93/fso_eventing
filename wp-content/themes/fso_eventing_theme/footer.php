8213fb0f332df550e5bad739abf9767ffeb6f2c9
Victoria <sirmalkom@gmail.com>
2017-01-10 16:30:47 +0100

    Footer wigedts

--- wp-content/themes/fso_eventing_theme/footer.php
+++ wp-content/themes/fso_eventing_theme/footer.php
@@ -1,4 +1,9 @@
-<footer class="">
+<footer class="fso-footer">
+	<?php if ( is_active_sidebar( 'footer-wigets' ) ) : ?>
+		<div id="footer-wigets" class="footer-wigets widget-area " role="complementary">
+			<?php dynamic_sidebar( 'footer-wigets' ); ?>
+		</div>
+	<?php endif; ?>
 </footer>
 </div>
 </body>
