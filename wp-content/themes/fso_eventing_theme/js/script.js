5c7d5f5a42ae5b5ab35414afad242737deef6c56
Victoria <sirmalkom@gmail.com>
2017-02-08 11:53:04 +0100

    Set height from current width for responsive same size images.

--- wp-content/themes/fso_eventing_theme/js/script.js
+++ wp-content/themes/fso_eventing_theme/js/script.js
@@ -2,18 +2,21 @@ $(document).ready(function(){
 
 	$('#menu-menu-1').addClass('nav navbar-nav');
 	$('.fancybox-thumbs').fancybox({
-							prevEffect : 'none',
-							nextEffect : 'none',
+			prevEffect : 'none',
+			nextEffect : 'none',
+			closeBtn  : true,
+			arrows    : true,
+			nextClick : true,
+			helpers : {
+					thumbs : {
+							width  : 50,
+							height : 50
+					}
+			}
+		});
 
-							closeBtn  : true,
-							arrows    : true,
-							nextClick : true,
+		$(window).on( 'resize', function () {
+			  $('.img').height( $('.img').width() / 1.7 );
+		}).resize();
 
-							helpers : {
-									thumbs : {
-											width  : 50,
-											height : 50
-									}
-							}
-					});
 });
