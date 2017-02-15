ba3f39d09df0da22b43b80e45b95838fe9ad21a0
Victoria <sirmalkom@gmail.com>
2017-02-15 11:52:41 +0100

    My Plugin: Load classes Register and Form

--- wp-content/plugins/mr_bruno_powered_by/mr-bruno-prowered-by.php
+++ wp-content/plugins/mr_bruno_powered_by/mr-bruno-prowered-by.php
@@ -8,3 +8,14 @@ Author URI: http://victoria-johansson.com/
 
 define('MBPB_PLUGIN_PATH', __FILE__);
 define('MBPB_PLUGIN_DIR_PATH', __DIR__);
+
+// Load classes
+$classes = array(
+  'MBPBRegister',
+  'MBPBForm'
+);
+
+foreach($classes as $class) {
+  include_once(MBPB_PLUGIN_DIR_PATH . '/classes/'.$class.'.class.php');
+  new $class();
+}
