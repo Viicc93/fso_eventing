71d40521a8063330d523c28e73c12a84e1bd5983
Victoria <sirmalkom@gmail.com>
2017-02-15 11:50:30 +0100

    My plugin: Create plugin and constant variables for paths

---
+++ wp-content/plugins/mr_bruno_powered_by/mr-bruno-prowered-by.php
@@ -0,0 +1,10 @@
+<?php
+/*Plugin Name: Mr. Bruno Bowered By
+Description: List your sponsors with images and links to thier websites.
+Version: 0.1
+Author: Mr. Bruno
+Author URI: http://victoria-johansson.com/
+*/
+
+define('MBPB_PLUGIN_PATH', __FILE__);
+define('MBPB_PLUGIN_DIR_PATH', __DIR__);
