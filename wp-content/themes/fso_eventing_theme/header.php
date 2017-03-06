c2ce14c67ee0f7ab26ff6d49c076c124a33ef5e3
Victoria <sirmalkom@gmail.com>
2017-03-06 10:11:02 +0100

    favicon

--- wp-content/themes/fso_eventing_theme/header.php
+++ wp-content/themes/fso_eventing_theme/header.php
@@ -2,11 +2,9 @@
 <html>
 
 <head>
-  <?php wp_head(); ?>
   <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
-  <!-- Add fancyBox -->
-  <link rel="stylesheet" href="/fso_eventing/wp-content/themes/fso_eventing_theme/js/fancybox/source/jquery.fancybox.css?v=2.1.6" type="text/css" media="screen" />
-  <script type="text/javascript" src="/fso_eventing/wp-content/themes/fso_eventing_theme/js/fancybox/source/jquery.fancybox.pack.js?v=2.1.6"></script>
+  <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/img/favicon.png" />
+  <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
